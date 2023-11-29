d27fdf28847ede657b3b9cb26ed128941ccb7f73
amal-techno <amal@technoexponent.com>
2023-11-29 12:07:59 +0530

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,20 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('live-kit-room')
+  createLiveKitRoom(@Body() param: any): any {
+    return this.appService._createRoom(param);
+  }
+
+  @Post('live-kit-token')
+  getLiveKitToken(@Body() param: any): any {
+    return this.appService._getLiveKitToken(param);
+  }
+
+  @Get('live-kit-url')
+  getLiveKitUrl(){
+    return this.appService._getLiveKitUrl();
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { AccessToken, Room, RoomServiceClient } from 'livekit-server-sdk';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async _createRoom(param) {
+
+    const livekitHost = process.env.LIVEKIT_URL;
+    const roomService = new RoomServiceClient(livekitHost, process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET);
+
+    const opts = {
+      name: param.roomName,
+      emptyTimeout: 10 * 60, // 10 minutes
+      maxParticipants: 20,
+    };
+    return await roomService.createRoom(opts)
+
+    // .then((room: Room) => {
+    //   console.log('room created', room);
+    // });
+
+  }
+
+
+  _getLiveKitToken(param: any) {
+    // if this room doesn't exist, it'll be automatically created when the first
+    // client joins
+    const roomName = param.roomName;
+    // identifier to be used for participant.
+    // it's available as LocalParticipant.identity with livekit-client SDK
+    const participantName = param.participantName;
+
+
+
+    const at = new AccessToken(process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET, {
+      identity: participantName,
+    });
+    at.addGrant({
+      roomJoin: true, room: roomName, canPublish: true,
+      canSubscribe: true,
+    });
+
+    const token = at.toJwt();
+    // console.log('access token', token);
+    return { liveKitToken: token, liveKitUrl: process.env.LIVEKIT_URL };
+  }
+
+  _getLiveKitUrl() {
+    return { liveKitUrl: process.env.LIVEKIT_URL }
   }
 }
